62d0918274cd2f7f26bdbe98058a8ea201973d5f
wenkrang <2934356978@qq.com>
2025-01-29 22:13:29 +0800

    Initial commit

---
+++ src/main/java/com/wenkrang/ecoError/EcoError.java
@@ -0,0 +1,17 @@
+package com.wenkrang.ecoError;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EcoError extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
