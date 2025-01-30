c190894b5ed2e575ad0082d772434dd7528edae3
Fangtang3 <158278525+Fangtang3@users.noreply.github.com>
2025-01-30 12:36:41 +0800

    只取消来自烟花，对玩家的弹射物伤害

--- src/main/java/com/wenkrang/ecoError/event/board/FireworkDamage.java
+++ src/main/java/com/wenkrang/ecoError/event/board/FireworkDamage.java
@@ -1,5 +1,6 @@
 package com.wenkrang.ecoError.event.board;
 
+import com.wenkrang.ecoError.lib.JavaUse;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,13 +10,17 @@ import javax.annotation.Nonnull;
 
 public final class FireworkDamage implements Listener {
     @EventHandler
+    @SuppressWarnings("all")
     public void onDamage(@Nonnull EntityDamageEvent event) {
         switch (event.getCause()) {
             case PROJECTILE:
-                if (event.getEntity().getType() == EntityType.PLAYER) {
-                    event.setCancelled(true);
-                } else if (false/*todo 判断被伤害实体是否为年兽*/) {
-                    event.setDamage(event.getDamage() * 5/*todo 伤害*/);
+                if (JavaUse.nn(event.getDamageSource().getDirectEntity()).getType()
+                        .equals(EntityType.FIREWORK_ROCKET)) {
+                    if (event.getEntity().getType() == EntityType.PLAYER) {
+                        event.setCancelled(true);
+                    } else if (false/*todo 判断被伤害实体是否为年兽*/) {
+                        event.setDamage(event.getDamage() * 5/*todo 伤害*/);
+                    }
                 }
             case ENTITY_ATTACK:
                 //todo
