42c3079bf2e22347e5a7f33ac4feb35e83bf71d2
Fangtang3 <158278525+Fangtang3@users.noreply.github.com>
2025-01-30 15:56:03 +0800

    task

--- src/main/java/com/wenkrang/ecoError/event/board/BoardPlayerJoin.java
+++ src/main/java/com/wenkrang/ecoError/event/board/BoardPlayerJoin.java
@@ -2,6 +2,7 @@ package com.wenkrang.ecoError.event.board;
 
 import com.wenkrang.ecoError.EcoError;
 import com.wenkrang.ecoError.gui.board.BoardGUI;
+import com.wenkrang.ecoError.lib.JavaUse;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -16,26 +17,24 @@ import java.io.IOException;
 public class BoardPlayerJoin implements Listener {
     @EventHandler
     public static void PlayerJoin(PlayerJoinEvent event) throws IOException, InvalidConfigurationException {
-        File file = new File("./plugins/EcoError/player/" + event.getPlayer().getUniqueId().toString() + ".yaml");
+        File file = new File("./plugins/EcoError/player/" + event.getPlayer().getUniqueId() + ".yaml");
         YamlConfiguration yamlConfiguration = new YamlConfiguration();
         if (!file.exists()) {
             yamlConfiguration.set("1", "no");
             yamlConfiguration.set("2", "no");
             yamlConfiguration.set("3", "no");
             yamlConfiguration.set("4", "no");
-            yamlConfiguration.save("./plugins/EcoError/player/" + event.getPlayer().getUniqueId().toString() + ".yaml");
+            yamlConfiguration.save("./plugins/EcoError/player/" + event.getPlayer().getUniqueId() + ".yaml");
         }
-        yamlConfiguration.load("./plugins/EcoError/player/" + event.getPlayer().getUniqueId().toString() + ".yaml");
-        if (!(yamlConfiguration.getString("1").equalsIgnoreCase("yes") && yamlConfiguration.getString("2").equalsIgnoreCase("yes") && yamlConfiguration.getString("3").equalsIgnoreCase("yes") && yamlConfiguration.getString("4").equalsIgnoreCase("yes"))) {
+        yamlConfiguration.load("./plugins/EcoError/player/" + event.getPlayer().getUniqueId() + ".yaml");
+        if (!(JavaUse.nn(yamlConfiguration.getString("1")).equalsIgnoreCase("yes") && yamlConfiguration.getString("2").equalsIgnoreCase("yes") && yamlConfiguration.getString("3").equalsIgnoreCase("yes") && yamlConfiguration.getString("4").equalsIgnoreCase("yes"))) {
 
             new BukkitRunnable() {
                 @Override
                 public void run() {
                     try {
-                        BoardGUI.openboard((Player) event.getPlayer());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    } catch (InvalidConfigurationException e) {
+                        BoardGUI.openboard(event.getPlayer());
+                    } catch (IOException | InvalidConfigurationException e) {
                         throw new RuntimeException(e);
                     }
 
